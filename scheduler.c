e464dba348b1a599e00d102d64da16658e7a4f44
Keith Yong <terda12@gmail.com>
2014-11-06 17:24:57 -0500

    basic input functionality

--- scheduler.c
+++ scheduler.c
@@ -1,6 +1,17 @@
-#include <assert.h>
+#include <stdio.h>
 
-int main()
+#define MAXINPUT        1000
+
+int main(int argc, char *argv[])
 {
+    int x;
+    int count = 0;
+    int input[MAXINPUT];
+    char c;
+    while (scanf("%d%c", &x, &c) != EOF){
+        input[count] = x;
+        count++;
+    }
+    printf("%d", input[1]);
     return 0;
 }
