ec2272398f8b35118c538026a3022c65c3dd5d52
Keith Yong <terda12@gmail.com>
2014-11-09 20:26:54 -0500

    added some qcpu scheduling logic for fcfs. Still a lot of work to be done.

--- scheduler.c
+++ scheduler.c
@@ -17,6 +17,9 @@ void printQueue(void);
 int findIndexWithMin(int * arr, int n);
 void fcfs();
 
+/* Used for for loops */
+int i, j;
+
 /* Arrays that input will go into */
 int input[MAX_LINES][MAX_INPUT];
 
@@ -26,16 +29,22 @@ int cpu[MAX_LINES][MAX_INPUT];
 /* The arrival time for ith process */
 int at[MAX_LINES];
 
+/* Running time of process */
+int runningTime[MAX_LINES][MAX_INPUT];
+
 /* The number of cpu bursts for ith process */
 int numcpu[MAX_LINES];
 
 /* Number of processes that was found */
 int numProcesses;
 
+/* At process [i], stores which state that the process
+ * is in, so if process[i] has completed one cpu burst
+ * then it is in state++ */
+int processState[MAX_LINES];
+
 int main(int argc, char *argv[])
 {
-    /* Used for for loops */
-    int i, j;
 
     /* Input block */
     {
@@ -78,7 +87,13 @@ int main(int argc, char *argv[])
         putchar('\n');
     }
     /* End of input block */
+
+    /* Initilize process state array.
+     * Every process starts at zero. */
+    for (i = 0; i < numProcesses; i++)
+        processState[i] = 0;
     
+    /* Do fcfs. TODO: Handle user shell input */
     fcfs();
     return 0;
 }
@@ -89,11 +104,56 @@ void fcfs()
     int firstArrivedProcess = findIndexWithMin(at, numProcesses);
 
     /* First process is actually 0, not 1 */
-    printf("First arrived process found: P%d\n", firstArrivedProcess + 1);
-    enqueue(firstArrivedProcess);
-    while (isQueueEmpty() != 1)
-    {
+    int time = 0;
+    int done = 0;
 
+    /* CPU is free at the beginning */
+    int isCPUFree = 1;
+    int currProcess;
+    int deq;
+    int dequeueRet;
+    int runUntil;
+    while (!done)
+    {
+        printf("Time: %d\n", time);
+
+        /* Just a dummy end time for now so it is not
+         * an infinite loop. */
+        if (time == 30){
+            done = 1;
+        }
+
+        /* Check for all arrival times, if current time
+         * is equal to arrival time then enqueue it */
+        for (i = 0; i < numProcesses; i++)
+        {
+            if (time == at[i])
+            {
+                printf("P%d has arrived\n", i + 1);
+                enqueue(i);
+            }
+        }
+
+        /* If the CPU is free */
+        if (isCPUFree == 1){
+            /* Attempt to dequeue from the ready queue */
+            dequeueRet = dequeue(&deq);
+            /* If dequeue is successful, run process deq*/
+            if (dequeueRet == 0){
+                printf("P%d is running\n", deq + 1);
+                runUntil = time + cpu[deq][processState[i]];
+                processState[i]++;
+                isCPUFree = 0;
+            }
+        }
+        
+
+        /* A process is finished its burst */
+        if (time == runUntil){
+            isCPUFree = 1;
+            printf("P%d done running.\n", deq + 1);
+        }
+        time++;
     }
     printQueue();
 }
@@ -106,7 +166,7 @@ int enqueue(int new)
     Queue[queueIn] = new;
     queueIn = (queueIn + 1) % QUEUE_SIZE;
 
-    printf("Enqueued P%d\n", new + 1);
+    printf("P%d put in queue\n", new + 1);
     return 0; /* Successfully enqueued */
 }
 
@@ -121,7 +181,8 @@ int dequeue(int * old)
     return 0; /* Successfully dequeued */
 }
 
-int isQueueEmpty(){
+int isQueueEmpty()
+{
     if (queueIn == queueOut)
         return 1;
 
@@ -140,12 +201,15 @@ void printQueue(void)
     putchar('\n');
 }
 
-int findIndexWithMin(int * arr, int len){
+int findIndexWithMin(int * arr, int len)
+{
     int i;
     int min = MAXINT;
     int minIndex = MAXINT;
-    for (i = 0; i < len; i++){
-        if (arr[i] < min){
+    for (i = 0; i < len; i++)
+    {
+        if (arr[i] < min)
+        {
             min = arr[i];
             minIndex = i;
         }
